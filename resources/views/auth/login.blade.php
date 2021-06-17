9b872b14cb38f23fa08dfd51788c99a17c8b3d47
zarszz <zarszzntunx@rocketmail.com>
2021-06-17 10:23:29 +0700

    fix: remove login by google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,10 +40,6 @@
                 </label>
             </div>
             <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-            <a href="{{ url('auth/google') }}  " class="btn btn-primary btn-social btn-block btn-lg shadow-lg mt-5">
-                <span class="bi bi-google"></span>
-                Login with Google
-            </a>
         </form>
         <div class="text-center mt-5 text-lg fs-4">
             @if (Route::has('register'))
